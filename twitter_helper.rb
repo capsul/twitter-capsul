05345c63f5b6910c73c69bc1d9b32ff5508233f5
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-08-20 20:42:09 -0700

    Remove parameters and clean up spacing

--- twitter_helper.rb
+++ twitter_helper.rb
@@ -3,34 +3,31 @@ require 'twitter'
 require 'json'
 
 module Tweets
-  def self.search(params = {})
+  def self.search params = {}
     twitter_fetcher = TwitterFetcher.new
-    twitter_fetcher.search_tweets(params)
+    twitter_fetcher.search_tweets params
   end
 
   class TwitterFetcher
-
-    def search_tweets(params = {})
+    def search_tweets params = {}
       client = config_client
 
       tweets = client.search("",
-                    geocode: params['lat'] + "," + 
-                      params['lng'] + "," + 
-                      ".20mi",
-                    :until => params['time'], # Ac
-                    result_type: 'recent',
-                    count: 100,
-                    include_entities: true 
-      ).map do |tweet|
-        tweet_to_granual(tweet)
-      end
-      filtered_tweets = filterDuplicates(tweets)
-      puts filtered_tweets.count
-      filtered_tweets.sort_by{ |key| key["id"].to_i } 
+        geocode: params['lat'] + "," + 
+                 params['lng'] + "," + 
+                 ".20mi",
+        :until => params['time'],
+        result_type: 'recent',
+        count: 100,
+        include_entities: true 
+        ).map do |tweet|
+          tweet_to_granual tweet
+        end
+      clean_response tweets
     end
     
     private
-    def config_client(args = {})
+    def config_client args = {}
       client = Twitter::REST::Client.new do |config|
         config.consumer_key         = ENV['CONSUMER_KEY']
         config.consumer_secret      = ENV['CONSUMER_SECRET']
@@ -39,11 +36,20 @@ module Tweets
       end
     end
 
-    def filterDuplicates(tweets)
+    def clean_response tweets
+      filtered_tweets = filter_duplicates tweets
+      sort_hash_by_type filtered_tweets "id"
+    end
+
+    def filter_duplicates tweets
       tweets.uniq { |tweet| [tweet[:author] ].join(":") }
     end
 
-    def tweet_to_granual(tweet)
+    def sort_hash_by_type tweets type
+      tweets.sort_by { |key| key[type].to_i } 
+    end
+
+    def tweet_to_granual tweet
       return {
         type:       'text',
         id:         tweet.id.to_s,
@@ -57,15 +63,15 @@ module Tweets
       }
     end
 
-    def location(tweet)
+    def location tweet
       {
-        latitude: tweet.geo.coordinates[0].to_s,
-        longitude: tweet.geo.coordinates[1].to_s
+        latitude:   tweet.geo.coordinates[0].to_s,
+        longitude:  tweet.geo.coordinates[1].to_s
       }
     end
 
-    def hashtags(tweet)
-      tweet.hashtags.map {|hashtag| hashtag.text }
+    def hashtags tweet
+      tweet.hashtags.map { |hashtag| hashtag.text }
     end
   end
 end
